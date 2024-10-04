9bbb9e47fdbc6a10d21d80ae049f741584148f68
Vhierdy Hafidz <vhierdy.verdi@gmail.com>
2024-10-05 03:29:55 +0700

    Create feature deleteTodo and testDeleteTodo

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -7,15 +7,20 @@ public class TodoApp {
 
     // Test Feature Here
     public static void testAddTodo() {
-        for (int i = 1; i <= 11; i++) {
+        for (int i = 1; i <= 5; i++) {
             addTodo("Coba " + i);
         }
+    }
 
+    public static void testDeleteTodo() {
+        testAddTodo();
+        deleteTodo(3);
+        deleteTodo(1);
         viewTodo();
     }
 
     public static void main(String[] args) {
-        testAddTodo();
+        testDeleteTodo();
     }
 
     public static void addTodo(String todo) {
@@ -51,6 +56,33 @@ public class TodoApp {
         for (int i = 0; i < todoList.length; i++) {
             if (todoList[i] != null) {
                 System.out.println((i + 1) + ". " + todoList[i]);
+            }
+        }
+    }
+
+    public static void deleteTodo(int id) {
+        if (id > 0) {
+            id -= 1;
+
+            for (int i = id; i < todoList.length; i++) {
+                if (i == todoList.length - 1) {
+                    todoList[i] = null;
+                } else {
+                    todoList[i] = todoList[i+1];
+                }
+            }
+        } else if (todoList[0] == null) {
+            System.out.println("Todolist now is empty, there is nothing to delete");
+        } else if (id <= 0) {
+            System.out.println("Todo is start from 1");
+        } else if (id > todoList.length) {
+            System.out.println("Cannot delete, todo length maximum number is " + todoList.length);
+        } else if (todoList[id - 1] == null) {
+            for (int i = 0; i < todoList.length; i++) {
+                if (todoList[i] == null) {
+                    System.out.printf("Cannot delete id: %d, because Todo number only %d", id, i);
+                    break;
+                }
             }
         }
     }
